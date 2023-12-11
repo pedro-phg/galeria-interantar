dfc774690b12c0bad545dfcbce3450b0e7303044
pedro-phg <pedro.h.adalberto@gmail.com>
2023-12-11 17:51:47 -0300

    feat: layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Picture from './components/picture/Picture.tsx';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Picture from "./components/picture/Picture.tsx";
+import NavBar from "./components/navbar/NavBar.tsx";
 
 function App() {
   const [photos, setPhotos] = useState([]);
@@ -8,10 +9,12 @@ function App() {
   useEffect(() => {
     const fetchPhotos = async () => {
       try {
-        const response = await axios.get('https://interantar.com/_functions/gallery');
+        const response = await axios.get(
+          "https://interantar.com/_functions/gallery"
+        );
         setPhotos(response.data.data);
       } catch (error) {
-        console.error('Erro ao obter as fotos:', error);
+        console.error("Erro ao obter as fotos:", error);
       }
     };
 
@@ -19,10 +22,19 @@ function App() {
   }, []);
 
   return (
-    <div className='content'>
-      {photos.map(photo => (
-        <Picture key={photo._id} photo={photo} />
-      ))}
+    <div className="content">
+      <NavBar />
+      <div className="pictures__grid">
+        {photos.map((photo) => (
+          <Picture key={photo._id} photo={photo} />
+        ))}
+        {photos.map((photo) => (
+          <Picture key={photo._id} photo={photo} />
+        ))}
+        {photos.map((photo) => (
+          <Picture key={photo._id} photo={photo} />
+        ))}
+      </div>
     </div>
   );
 }
